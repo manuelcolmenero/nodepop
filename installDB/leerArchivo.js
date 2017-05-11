7dbc228f205f1fe65e99a6e356fadacd57e2149d
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2017-05-11 18:55:38 +0200

    Actualización de script de instalación de Base de Datos Creación de fichero de arranque de usuarios Creación de módulo de lectura de ficheros

---
+++ installDB/leerArchivo.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const fs = require('fs'); // Se importa la libreria fs para leer ficheros
+const path = require('path');
+
+// ------- Funciones
+
+function leerArchivo(nombreFichero, callback) {
+    const fichero = path.join('./installDB', nombreFichero);
+    let datosJSON ={};
+
+    fs.readFile(fichero, (err, data) => {
+      if(err) {
+        return callback (err);
+      } 
+        // Se controla el error sincrono de JSON
+        try {
+          // Se parsea el contenido del fichero y se convierte en un objeto
+          datosJSON = JSON.parse(data);
+
+        } catch (err) {
+            return callback(err);
+            
+        }
+
+        // Se retorna null como error y la versión para dar el proceso acabado como OK
+        callback (null, datosJSON);
+    });
+}
+
+// ------------- Area: Retorno -------------
+module.exports = leerArchivo;
--- install_db.js
+++ install_db.js
@@ -2,13 +2,10 @@
 
 // ------------------ Area: Declaraciones 
 // ------- require y asignaciones
-// Se llama a BBDD y se conecta
-//require('./lib/connectMongoose');
 
-// Se importa la libreria fs para leer ficheros
-const fs = require('fs');
-
-// ------- Funciones
+//require('./lib/connectMongoose'); // Se llama a BBDD y se conecta
+const async = require('async');
+const leerArchivo = require('./installDB/leerArchivo');
 
 // ------- Borrado 
 
@@ -19,19 +16,34 @@ const fs = require('fs');
 
 
 // ------- Regenerar tabla 
-fs.readFile('./installDB/initAnuncios.json', (err, data) => {
-  if(err) {
-    console.log('error: ', err);
-  } else {
-    let parseJSON = JSON.parse(data);
-    for (let indice = 0; indice < parseJSON.anuncios.length ; indice++) {
-        console.log('anuncio' + indice, parseJSON.anuncios[indice].nombre);
-        console.log('anuncio' + indice, parseJSON.anuncios[indice].venta);
-        console.log('anuncio' + indice, parseJSON.anuncios[indice].precio);
-        console.log('anuncio' + indice, parseJSON.anuncios[indice].foto);
-        console.log('anuncio' + indice, parseJSON.anuncios[indice].tags);
-    }
+
+
+// ------- Proceso
+leerArchivo ('initAnuncios.json', (err, datosJSON) => {
+  if (err) {
+    console.log ('Error:', err);
+    return;
   }
+  for (let indice = 0; indice < datosJSON.anuncios.length ; indice++) {
+        console.log('anuncio' + indice, datosJSON.anuncios[indice].nombre);
+        console.log('anuncio' + indice, datosJSON.anuncios[indice].venta);
+        console.log('anuncio' + indice, datosJSON.anuncios[indice].precio);
+        console.log('anuncio' + indice, datosJSON.anuncios[indice].foto);
+        console.log('anuncio' + indice, datosJSON.anuncios[indice].tags);
+    }
+});
+
+leerArchivo ('initUsuarios.json', (err, datosJSON) => {
+  if (err) {
+    console.log ('Error:', err);
+    return;
+  }
+  for (let indice = 0; indice < datosJSON.usuarios.length ; indice++) {
+        console.log('usuario' + indice, datosJSON.usuarios[indice].nombre);
+        console.log('usuario' + indice, datosJSON.usuarios[indice].email);
+        console.log('usuario' + indice, datosJSON.usuarios[indice].clave);
+
+    }
 });
 
 
